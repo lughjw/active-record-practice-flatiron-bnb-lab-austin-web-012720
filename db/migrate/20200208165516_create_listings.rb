ddd9c39e9a150aef53e5d20284667aa40cd5d6b8
Lugh Wilson <lughjw@gmail.com>
2020-02-08 18:09:35 +0000

    Automatically backed up by Learn

--- db/migrate/20200208165516_create_listings.rb
+++ db/migrate/20200208165516_create_listings.rb
@@ -1,7 +1,11 @@
 class CreateListings < ActiveRecord::Migration[5.0]
   def change
     create_table :listings do |t|
-      t.
+      t.string :address
+      t.string :listing_type
+      t.string :title
+      t.text :description
+      t.float :price
     end
   end
 end
