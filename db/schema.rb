c4d4812d91c7fd16db3a7114d47ee0fda153ab29
Lugh Wilson <lughjw@gmail.com>
2020-02-08 17:29:28 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200208165602) do
+ActiveRecord::Schema.define(version: 20200208165546) do
 
   create_table "city", force: :cascade do |t|
     t.string   "name"
