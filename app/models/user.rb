48d4f1f903e187b50fce3dfb7b1482f101c95f6e
Lugh Wilson <lughjw@gmail.com>
2020-02-08 18:48:46 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
   # creates the method listings which looks through Listing.all for all rows which have the host_id = id and pulls out those listings
   has_many :listings, foreign_key:'host_id'
-  # creates the reservations method which does the exact same thing as the listings method above, just named differently. We have to use the through because there is no table called reservations 
+  # creates the reservations method which does the exact same thing as the listings method above, just named differently. We have to use the through because there is no table called reservations
   has_many :reservations, through: :listings, foreign_key:'host_id'
 
   # creates the reviews method which searches Review.all for all rows which have the guest_id = id and pulls out those reviews
