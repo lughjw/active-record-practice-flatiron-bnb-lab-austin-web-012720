46e26d9c0ceedd7d30ea91f97075cdd22cf4c161
Lugh Wilson <lughjw@gmail.com>
2020-02-08 19:00:20 +0000

    Automatically backed up by Learn

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ActiveRecord::Base
+  # Need to put the class_name: :User because the guest_id is the id of a User
   belongs_to :guest, class_name: :User
   belongs_to :reservation
 end
