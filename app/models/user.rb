b75e33d2795f1a6292e1908c030f1fda04a47224
Lugh Wilson <lughjw@gmail.com>
2020-02-08 18:31:06 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :listings, foreign_key:"host_id"
+  has_many :listings, foreign_key:'host_id'
   has_many :reservations, through: :listings, foreign_key:'host_id'
   has_many :reviews, foreign_key: 'guest_id'
   has_many :trips, class_name: :Reservation, foreign_key: 'guest_id'
